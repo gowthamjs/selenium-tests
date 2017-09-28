17455c3ec547ed76f2fb58b09b89ec01dc4cf810
Gowtham J S <gowthamjs@yahoo.com>
2017-09-27 20:58:27 -0700

    Add elements for search box and search icon in the home screen

--- src/main/java/com/build/qa/build/selenium/pageobjects/homepage/HomePage.java
+++ src/main/java/com/build/qa/build/selenium/pageobjects/homepage/HomePage.java
@@ -2,6 +2,7 @@ package com.build.qa.build.selenium.pageobjects.homepage;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 
@@ -10,6 +11,8 @@ import com.build.qa.build.selenium.pageobjects.BasePage;
 public class HomePage extends BasePage {
 	
 	private By buildThemeBody;
+	private By searchBox = By.id("search_txt");
+	private By searchIcon = By.className("button-primary search-site-search");
 	
 	public HomePage(WebDriver driver, Wait<WebDriver> wait) {
 		super(driver, wait);
@@ -18,5 +21,13 @@ public class HomePage extends BasePage {
 	
 	public boolean onBuildTheme() { 
 		return wait.until(ExpectedConditions.presenceOfElementLocated(buildThemeBody)) != null;
+	}
+	
+	public WebElement searchBox() {
+		return driver.findElement(searchBox);
+	}
+	
+	public WebElement searchIcon() {
+		return driver.findElement(searchIcon);
 	}
 }
