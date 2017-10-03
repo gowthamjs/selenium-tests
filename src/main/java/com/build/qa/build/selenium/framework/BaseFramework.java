fad2deda5b64182e1b52be15f6b25a6e5bd2fa51
Gowtham J S <gowthamjs@yahoo.com>
2017-10-02 23:52:54 -0700

    Update with some comments and driver path

--- src/main/java/com/build/qa/build/selenium/framework/BaseFramework.java
+++ src/main/java/com/build/qa/build/selenium/framework/BaseFramework.java
@@ -3,6 +3,8 @@ package com.build.qa.build.selenium.framework;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -16,10 +18,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public abstract class BaseFramework {
 	protected WebDriver driver;
@@ -28,6 +36,9 @@ public abstract class BaseFramework {
 	private static final String CONFIG_FILE = "./conf/automation.properties";
 	private static final String DRIVER_FIREFOX = "firefox";
 	private static final String DRIVER_CHROME = "chrome";
+	private static final String DRIVER_SAFARI = "safari";
+	private static final String DRIVER_CHROME_MOBILE = "mobile-chrome";
+
 	private static Properties configuration;
 
 	@Rule
@@ -45,15 +56,48 @@ public abstract class BaseFramework {
 	}
 
 	@Before
-	public void setUpBefore() {
-		DesiredCapabilities capabilities;
+	public void setUpBefore() throws MalformedURLException {
+		DesiredCapabilities capabilities = null;
 		// Which driver to use? 
 		if (DRIVER_CHROME.equalsIgnoreCase(configuration.getProperty("BROWSER"))) {
+			
+			/*
+			 * uncomment and add the path of chrome driver in your machine to run on Chrome driver.
+			 * Running the script on chrome browser displays captcha page.
+			 */
+		
+			//System.setProperty("webdriver.chrome.driver", "C:/chromedriver/chromedriver.exe");
 			capabilities = DesiredCapabilities.chrome();
 			driver = new ChromeDriver(capabilities);
+			
 		} else if (DRIVER_FIREFOX.equalsIgnoreCase(configuration.getProperty("BROWSER"))) {
 			capabilities = DesiredCapabilities.firefox();
+			//uncomment and add the path of gecko driver in your machine to run on FireFox driver
+			//System.setProperty("webdriver.gecko.driver", "C:/geckodriver/geckodriver.exe");
 			driver = new FirefoxDriver(capabilities);
+			
+		} else if (DRIVER_SAFARI.equalsIgnoreCase(configuration.getProperty("BROWSER"))) {
+			capabilities = DesiredCapabilities.safari();
+			
+			/*
+			 * I am working on a windows machine and it appears Apple doesn't support running tests on windows mac.
+			 * Here is the documentation link: https://github.com/SeleniumHQ/selenium/wiki/SafariDriver
+			 */
+			
+			driver = new SafariDriver(capabilities);
+			
+		} else if (DRIVER_CHROME_MOBILE.equalsIgnoreCase(configuration.getProperty("BROWSER"))) {
+			capabilities = new DesiredCapabilities();
+			
+			/*
+			 * Update the device name in the below statement.
+			 * In the mobile device chrome browser also, captcha screen displays
+			 * Sorry, I couldn't check this in iOS, as I have windows machine
+			 */
+			
+			capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "device");
+			capabilities.setCapability(AndroidMobileCapabilityType.BROWSER_NAME, "Chrome");
+			driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
 		}
 		// Define fluent wait
 		wait = new FluentWait<WebDriver>(driver).withTimeout(15, TimeUnit.SECONDS).pollingEvery(500, TimeUnit.MILLISECONDS)
